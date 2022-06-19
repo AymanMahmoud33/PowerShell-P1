7330e75bb5d1ed27d7d9133b64a5557f443f2bcd
AymanMahmoud33 <49797373+AymanMahmoud33@users.noreply.github.com>
2022-06-19 18:27:25 +0200

    Update Getting Help.ps1

--- Getting Help.ps1
+++ Getting Help.ps1
@@ -1,12 +1,12 @@
 Getting Help
 Update-Help
-PowerShell installs with minimal help information 
-Update-Help downloads the latest information 
-Built-in limitation of once per 24 hours (-Force)
+# PowerShell installs with minimal help information 
+# Update-Help downloads the latest information 
+# Built-in limitation of once per 24 hours (-Force)
 
 Save-Help
-Great for keeping help updated on machines without internet access of behind firewalls 
-Save-Help to \\share, Update-Help from \\share
+# Great for keeping help updated on machines without internet access of behind firewalls 
+# Save-Help to \\share, Update-Help from \\share
 
 Get-Help
 Get-Help <Command> returns help file 
@@ -17,7 +17,7 @@ About topics are conceptual help topics
 You can use Get-Help as a search engine 
 #>
 # you can’t mix parameters from different parameter sets 
->Get-Help Get-Command –Examples 
->Get-Help Get-Command –Examples –Parameter
+Get-Help Get-Command –Examples 
+Get-Help Get-Command –Examples –Parameter
 # Positional parameters has [] about the parameter name like:
 Get-Help [[-Name] <string>]
